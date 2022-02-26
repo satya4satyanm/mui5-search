3fcc05d80e5b27b1476ba5a3ed8774efadd03ef2
Mishra <satyanarayan.mishra@pearson.com>
2022-02-26 13:24:41 +0530

    wip setup

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-exports.mui5search = function() {
-    console.log("This is a message from mui 5 search.");
-  }
+import React from 'react'
+import ClearIcon from '@mui/icons-material/Clear'
+import SearchIcon from '@mui/icons-material/Search'
+
+exports.mui5search = function () {
+  console.log('This is a message from mui 5 search.')
+
+  return (
+    <>
+      <ClearIcon></ClearIcon>
+      <SearchIcon></SearchIcon>
+    </>
+  )
+}

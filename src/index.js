1b987da98fa73afcb8d02d101c708e907d61c36c
Mishra <satyanarayan.mishra@pearson.com>
2022-02-26 13:47:04 +0530

    code cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ClearIcon from "@mui/icons-material/Clear";
 import SearchIcon from "@mui/icons-material/Search";
 
-exports.mui5search = function () {
+exports.Mui5search = function () {
   console.log("This is a message from mui 5 search.");
 
   return (
